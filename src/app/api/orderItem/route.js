8dd1d9d48af3f4a9845d6b99b9c206a6daf4e503
Paul Hsueh <hsueh.paul@gmail.com>
2024-01-16 13:07:37 -0500

    added routes and made frontend changes

--- src/app/api/orderItem/route.js
+++ src/app/api/orderItem/route.js
@@ -1,21 +1,29 @@
-import prisma from "../../../../lib/prisma"
+import prisma from "../../../../lib/prisma";
 
 export async function GET() {
-    const res = await prisma.orderItem.findMany()
-    const orderItem = await res
-    return Response.json({ orderItem })
-  }
+  const res = await prisma.orderItem.findMany();
+  const orderItem = await res;
+  return Response.json({ orderItem });
+}
 
-export async function POST(req) {
-    const { burrito, quantity, order, orderId } = req.json()
-
-    const result = await prisma.orderItem.create({
-        data: {
-            burrito,
-            quantity,
-            order,
-            orderId
-        }
+export async function POST(request) {
+  const orderItem = await request.json();
+  const {burrito, quantity, orderId} = orderItem
+  try { 
+    const createOrderItem = await prisma.orderItem.create({
+      data: {
+        burrito: burrito,
+        quanity: quantity,
+        orderId: orderId
+      },
     });
-    res.json(result)
+    return new Response("order item created", {
+      status: 201,
+    });
+  } catch (error) {
+    console.log("error creating orderItem", error)
+    return new Response("order item not created", {
+      status: 500,
+    });
+  }
 }
